e9c1b425141c61354f2f18ff3cb82a71fac4e3f7
Fábio Rogério <fabio@tindin.com.br>
2018-10-25 20:52:39 -0300

    add filtro por id

--- functions/posts.js
+++ functions/posts.js
@@ -1,11 +1,26 @@
 const mongodb = require('../lib/mongodb')
+const ObjectId = require('mongodb').ObjectId
 const util = require('../lib/util')
 const tablePosts = 'posts'
+const tableContributors = 'contributors'
 
 module.exports = async (event, context) => {
+  const params = event.queryStringParameters || {}
+  console.log(params)
+
   try {
     await mongodb.connect()
-    var data = await mongodb(tablePosts).find({}).toArray()
+    const filter = {}
+    var data = null
+
+    if (params.id) {
+      filter._id = ObjectId(params.id)
+      data = await mongodb(tablePosts).findOne(filter)
+      data.contributor = await mongodb(tableContributors).findOne({_id: data.contributor})
+    } else {
+      data = await mongodb(tablePosts).find(filter).toArray()
+    }
+
     return util.bind({ message: 'success listed', data: data })
   } catch (error) {
     if (error) return util.bind(error)
